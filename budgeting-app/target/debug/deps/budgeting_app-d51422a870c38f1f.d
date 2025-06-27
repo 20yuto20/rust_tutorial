4694c3bb0318d4af4b55526e76d1895e0bca5c0c
Yuto Kohata <yuto02618@gmail.com>
2025-06-28 02:19:38 +0900

    add

---
+++ budgeting-app/target/debug/deps/budgeting_app-d51422a870c38f1f.d
@@ -0,0 +1,5 @@
+/Users/yutokohata/rust_tutorial/budgeting-app/target/debug/deps/libbudgeting_app-d51422a870c38f1f.rmeta: src/main.rs
+
+/Users/yutokohata/rust_tutorial/budgeting-app/target/debug/deps/budgeting_app-d51422a870c38f1f.d: src/main.rs
+
+src/main.rs:
---
+++ budgeting-app/target/debug/deps/budgeting_app-f9e7e39a69cfb9c8.d
@@ -0,0 +1,5 @@
+/Users/yutokohata/rust_tutorial/budgeting-app/target/debug/deps/libbudgeting_app-f9e7e39a69cfb9c8.rmeta: src/main.rs
+
+/Users/yutokohata/rust_tutorial/budgeting-app/target/debug/deps/budgeting_app-f9e7e39a69cfb9c8.d: src/main.rs
+
+src/main.rs:
